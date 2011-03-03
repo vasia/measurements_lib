522f1fcbd0754b63b66d08cfa184b1e5c3104e80
vasia <vasilikikalavri@gmail.com>
2011-03-03 16:47:15 +0100

    seems like working

--- profiling.h
+++ profiling.h
@@ -30,8 +30,6 @@ FILE** f_array;		//array of file descriptors
 int* cur_pos_array;	//array of current positions
 struct event *buff_array;	//array of the thread's buffers
 
-struct event *buff;	//the thread's buffer
-//int cur_pos;		//current position in the buffer
 int ext_buff_size;	//global variable for buffer size
 
 /*Library initialization*/
@@ -60,12 +58,6 @@ int init_profiling(int buff_size, char* file_prefix, int num_threads){
 		return -1;
 	}
 
-	//allocate buffer
-	/*if((buff = malloc(buff_size*sizeof(e))) == NULL){
-		printf("out of memory");
-		return -1;
-	}*/
-
 	//initialize buffer pointers and open files
 	for(i=0; i<num_threads; i++){
 
@@ -83,18 +75,6 @@ int init_profiling(int buff_size, char* file_prefix, int num_threads){
 			fprintf(f_array[i], "ThreadID: [%u]\n", i);
 		}
 	}
-
-
-
-	/*if((f = fopen(strcat(file_prefix, file_suf), "w"))== NULL){
-		printf("fopen error");
-		return -1;
-	}
-	else{
-		fprintf(f, "Initializing Time: [%lu]\n", getticks());
-		fprintf(f, "ThreadID: [%u]\n", pthread_self());
-		return 0;
-	}*/
 	return 0;
 }
 
@@ -110,23 +90,17 @@ void flush(int i){
 		fprintf(f_array[i], "%10d\n" , buff_array[(i*ext_buff_size) + j].event_value);
 	}
 
-
-	/*for(i=0; i<ext_buff_size; i++){
-		fprintf(f, "%10lu\t", buff[i].timestamp);
-		fprintf(f, "%10d\t%10d\n" , buff[i].event_type, buff[i].event_value); 
-		}*/
-
 	//initialize buffer & buffer pointer
 	cur_pos_array[i] = 0;
-	/*for(j=0; j<ext_buff_size; j++){
-		buff_array[(i*ext_buff_size)].timestamp = 0;
-		buff_array[(i*ext_buff_size)].event_type = 0;
-		buff_array[(i*ext_buff_size)].event_value = 0;
-	}*/
+	for(j=0; j<ext_buff_size; j++){
+		buff_array[(i*ext_buff_size) + j].timestamp = 0;
+		buff_array[(i*ext_buff_size) + j].event_type = 0;
+		buff_array[(i*ext_buff_size) + j].event_value = 0;
+	}
 }
 
 /* record an event*/
-void record_event(int type, int value, int i, int num_threads){
+void record_event(int type, int value, int i){
 
 	//check if buffer is full
 	if(cur_pos_array[i] == ext_buff_size){	
@@ -138,11 +112,6 @@ void record_event(int type, int value, int i, int num_threads){
 		buff_array[(i*ext_buff_size) + cur_pos_array[i]].timestamp = getticks();
 		buff_array[(i*ext_buff_size) + cur_pos_array[i]].event_type = type;
 		buff_array[(i*ext_buff_size) + cur_pos_array[i]].event_value = value;
-
-		//if not, save event in the buffer
-		/*buff[cur_pos].timestamp = getticks();
-		buff[cur_pos].event_type = type;
-		buff[cur_pos].event_value = value;*/
 
 		//increase buffer pointer
 		cur_pos_array[i]++;
@@ -157,7 +126,6 @@ int finalize_profiling(int num_threads){
 		flush(i);
 
 	//close files
-	//open files
 	for(i=0; i<num_threads; i++){
 		if( (fclose(f_array[i]))!= 0){
 			printf("fclose error");
